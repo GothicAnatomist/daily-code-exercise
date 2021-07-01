20a9812a51da42e166cdd8acd20bdd397c1342bb
James Walker <james@gothicanatomist.co.uk>
2021-07-01 13:39:03 +0100

    [ADD] Solution to py-hello-world
    My solution to the py-hello-world challenge found on HackerRank at
    https://www.hackerrank.com/challenges/py-hello-world/problem

---
+++ say_hello_world.py
@@ -0,0 +1,12 @@
+# =====================================================================
+# Author:       James Walker (GothicAnatomist)
+# Create date:  01/07/2021
+# Site:         HackerRank
+# URL:          https://www.hackerrank.com/challenges/py-hello-world/problem
+# Description:  Solution to "Say "Hello, World!" With Python"
+# Licence:      CC BY 4.0
+# =====================================================================
+
+if __name__ == '__main__':
+    greeting_message = "Hello, World!"
+    print(greeting_message)
