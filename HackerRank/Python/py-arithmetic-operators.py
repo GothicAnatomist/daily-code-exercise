f2ea4b68389fa213ae9e43a2559a5030020be840
James Walker <james@gothicanatomist.co.uk>
2021-07-03 10:42:24 +0100

    [ADD] Solution to py-arithmetic_operators
    My solution to the py-arithmetic_operators challenge found on HackerRank at
    https://www.hackerrank.com/challenges/python-arithmetic-operators/problem

---
+++ HackerRank/Python/py-arithmetic-operators.py
@@ -0,0 +1,16 @@
+# =====================================================================
+# Author:       James Walker (GothicAnatomist)
+# Create date:  03/07/2021
+# Site:         HackerRank
+# URL:          https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+# Description:  Solution to "Arithmetic Operators"
+# Licence:      CC BY 4.0
+# =====================================================================
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a + b)
+    print(a - b)
+    print(a * b)
